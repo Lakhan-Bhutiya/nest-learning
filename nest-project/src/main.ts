3c69e4ea3a6365b63e47f253816e750541c78226
Lakhan-Bhutiya <bhutiyalakhan13@gmail.com>
2026-01-13 16:02:32 +0530

    just update something

--- nest-project/src/main.ts
+++ nest-project/src/main.ts
@@ -8,11 +8,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-    .setTitle('Basic projecy')
-    .setDescription('api documantation')
+    .setTitle('Basic project')
+    .setDescription('api documentation')
     .addBearerAuth()
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app,config)
   SwaggerModule.setup('/api',app,document)
   
